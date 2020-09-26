912d8afbecf13c6b4205e3cda1a9f7a6f94ac13d
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-09-26 11:24:36 +0300

    Frontend: SideBar (part)

---
+++ CoffeeCrmApp.frontend/src/components/SideBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <input type="checkbox" id="nav-toggle" hidden>
+        <nav class="nav">
+            <label for="nav-toggle" class="nav-toggle" onclick></label>
+            <h2 class="logo"> 
+                <img alt="Vue logo" src="../assets/logo.png" />
+            </h2>
+            <ul>
+                <li><a href="#1">Один</a></li>
+                <li><a href="#2">Два</a></li>
+                <li><a href="#3">Три</a></li>
+                <li><a href="#4">Четыре</a></li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component({
+  name: "SideBar"
+})
+  export default class SideBar extends Vue {}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ CoffeeCrmApp.frontend/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="main">
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {}
+};
+</script>
+
+<style lang="scss">
+  img {
+    margin-top: 20px;
+    margin-left: 50px;
+    width: 120px;
+    height: 70px;
+  }
+</style>
